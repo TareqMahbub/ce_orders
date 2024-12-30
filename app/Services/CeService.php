d90cdf74dac04f1feecd821ca938ba14825ed154
TareqMahbubSettly <tareq@settly.nl>
2024-12-30 17:40:46 +0100

    Adds ce service and config file

---
+++ app/Services/CeService.php
@@ -0,0 +1,145 @@
+<?php
+
+namespace App\Services;
+
+use Exception;
+use App\Traits\HasMakeAble;
+use Illuminate\Http\Client\ConnectionException;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Log;
+use RuntimeException;
+
+class CeService
+{
+    use HasMakeAble;
+
+    /**
+     * @param  array  $response
+     * @return bool
+     */
+    private function isRequestOk(array $response): bool
+    {
+        return array_key_exists('StatusCode', $response) && $response['StatusCode'] === 200;
+    }
+
+    /**
+     * @param  int  $page
+     * @param  array  $statuses
+     * @return array
+     */
+    public function fetchPagedOrders(int $page = 1, array $statuses = ['NEW']): array
+    {
+        $url = config('ce.api.endpoint') .  "/v2/orders";
+        $params = [
+            'apikey' => config('ce.api.key'),
+            'statuses' => $statuses,
+            'page' => $page
+        ];
+
+        if (($responseObject = Http::get($url, $params))->successful()) {
+            $response = $responseObject->json();
+
+            if ($this->isRequestOk($response)) {
+                return $response;
+            }
+        }
+
+        return [];
+    }
+
+    /**
+     * @param  int  $stockLocationId
+     * @param  int  $pageIndex
+     * @param  int  $pageSize
+     * @return array
+     */
+    public function getPagedStocks(int $stockLocationId, int $pageIndex = 0, int $pageSize = 100): array
+    {
+        $url = config('ce.api.endpoint') . "/v2/offer/stock";
+        $params = [
+            'apikey' => config('ce.api.key'),
+            'stockLocationIds' => $stockLocationId,
+            'pageIndex' => $pageIndex,
+            'pageSize' => $pageSize,
+        ];
+
+        if (($responseObject = Http::get($url, $params))->successful()) {
+            $response = $responseObject->json();
+
+            if (array_key_exists('Content', $response) && $this->isRequestOk($response)) {
+                $items = $responseObject->json()['Content'];
+
+                if(!empty($items) && is_array($items)) {
+                    return $items;
+                }
+            }
+        }
+
+        throw new RuntimeException('Failed to get the stocks');
+    }
+
+    /**
+     * @param  string  $merchantProductNo
+     * @param  int  $stockLocationId
+     * @param  int  $stock
+     * @return bool
+     */
+    public function setStock(string $merchantProductNo, int $stockLocationId, int $stock): bool
+    {
+        try {
+            $url = config('ce.api.endpoint') . "/v2/offer/stock";
+            $params = ['apikey' => config('ce.api.key')];
+            $data = [
+                "MerchantProductNo" => $merchantProductNo,
+                "StockLocations" => [
+                    [
+                        "Stock" => $stock,
+                        "StockLocationId" => $stockLocationId
+                    ]
+                ]
+            ];
+
+            if (($responseObject = Http::withQueryParameters($params)->put($url, [$data]))->successful()) {
+                $response = $responseObject->json();
+
+                if ($this->isRequestOk($response)) {
+                    return true;
+                }
+            }
+        } catch (ConnectionException|Exception $e) {
+            $errorMessage = $e->getMessage() . ', Line: ' . $e->getLine() . ', File: ' . $e->getFile();
+            Log::error(
+                "Failed to update merchant product stock. Exception: $errorMessage",
+                [
+                    'merchant_product_no' => $merchantProductNo,
+                    'stock_location_id' => $stockLocationId,
+                    'stock' => $stock
+                ]
+            );
+        }
+
+        return false;
+    }
+
+    /**
+     * @param  string  $merchantProductNo
+     * @return array
+     */
+    public function getProduct(string $merchantProductNo): array
+    {
+        $url = config('ce.api.endpoint') .  "/v2/products/$merchantProductNo";
+        $params = [
+            'apikey' => config('ce.api.key'),
+        ];
+
+        if (($responseObject = Http::get($url, $params))->successful()) {
+            $response = $responseObject->json();
+
+            if ($this->isRequestOk($response)) {
+                return $response;
+            }
+        }
+
+        return [];
+    }
+}
---
+++ config/ce.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+    'api' => [
+        'endpoint' => env('CE_API_URL', 'https://api-dev.channelengine.net/api'),
+        'key' => env('CE_API_KEY', '541b989ef78ccb1bad630ea5b85c6ebff9ca3322'),
+    ],
+];
